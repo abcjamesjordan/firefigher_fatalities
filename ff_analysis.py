655aa6f36583e4f45f764f991a786a7682a765d8
abcjamesjordan <abcjamesjordan@gmail.com>
2021-02-13 13:34:56 -0700

    Update with png files

--- ff_analysis.py
+++ ff_analysis.py
@@ -156,4 +156,145 @@ ax1.axis('equal')
 ax1.set_title('Fatalities of 9/11', fontweight='bold', fontsize=18)
 
 plt.tight_layout()
+plt.savefig('911_pie_chart.png')
+
+cat_names = ['rank', 'classification', 'cause_of_death', 'nature_of_death', 'activity', 'emergency', 'duty', 'property_type', 'memorial_fund_info']
+cont_names = ['age', 'incident_date', 'date_of_death']
+
+print('Number of categorial columns: ', len(cat_names))
+print('Number of continous columns: ', len(cont_names))
+
+# Ranks
+# Get only top 10 ranks from dataset
+top_10_rank = df['rank'].value_counts()[:10]
+top_10_list = top_10_rank.index
+top_10_list
+top_10 = df.loc[df['rank'].isin(top_10_list)]
+
+# Plot using seaborn
+g = sns.catplot(x='rank', kind='count', palette='hls', data=top_10, height=6, aspect=10/6)
+plt.title('Top 10 Ranks', fontsize=15)
+plt.xlabel('Rank')
+plt.ylabel('Percent')
+[plt.setp(ax.get_xticklabels(), rotation=30) for ax in g.axes.flat];
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012, decimals=0))
+plt.savefig('ranks.png', bbox_inches='tight', pad_inches=0.25)
+
+# Classification
+g = sns.catplot(x='classification', kind='count', palette='hls', data=df, height=6, aspect=10/6)
+plt.title('Classifications of Fatalities', fontsize=18)
+plt.xlabel('Class of Firefighter')
+plt.ylabel('Percent')
+[plt.setp(ax.get_xticklabels(), rotation=30) for ax in g.axes.flat];
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012))
+plt.savefig('classifications.png', bbox_inches='tight', pad_inches=0.25)
+
+# Cause of Death
+# Quick clean up of the cause of death names
+def cod_clean(rank):
+    rank = rank.replace('Vehicle Collision - Includes Aircraft', 'Vehicle Collision')
+    rank = rank.replace('Caught or Trapped', 'Caught/Trapped')
+    return rank
+
+df['cause_of_death'] = [cod_clean(x) for x in df['cause_of_death']]
+
+g = sns.catplot(x='cause_of_death', kind='count', palette='hls', data=df, height=6, aspect=10/6)
+plt.title('Causes of Firefighter Deaths', fontsize=18)
+plt.xlabel('Cause of Death')
+plt.ylabel('Percent')
+[plt.setp(ax.get_xticklabels(), rotation=30) for ax in g.axes.flat];
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012))
+plt.savefig('cause_of_death.png', bbox_inches='tight', pad_inches=0.25)
+
+# Nature of Death
+def nod_clean(rank):
+    rank = rank.replace('Cerebrovascular Accident', 'Stroke/CVA')
+    return rank
+
+df['nature_of_death'] = [nod_clean(x) for x in df['nature_of_death']]
+
+g = sns.catplot(x='nature_of_death', kind='count', palette='hls', data=df, height=6, aspect=10/6)
+plt.title('Nature of Firefighter Deaths', fontsize=18)
+plt.xlabel('Nature of Death')
+plt.ylabel('Percent')
+[plt.setp(ax.get_xticklabels(), rotation=30) for ax in g.axes.flat];
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012, decimals=0))
+plt.savefig('nature_of_death.png', bbox_inches='tight', pad_inches=0.25)
+
+# Activity
+g = sns.catplot(x='activity', kind='count', palette='hls', data=df, height=6, aspect=10/6)
+plt.title('Activity During Incident', fontsize=18)
+plt.xlabel('Activity')
+plt.ylabel('Percent')
+[plt.setp(ax.get_xticklabels(), rotation=90) for ax in g.axes.flat];
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012, decimals=0))
+plt.savefig('activity.png', bbox_inches='tight', pad_inches=0.25)
+
+# Emergency
+g = sns.catplot(x='emergency', kind='count', palette='hls', data=df, height=6, aspect=10/6)
+plt.title('Emergency Status', fontsize=18)
+plt.xlabel('Emergency')
+plt.ylabel('Percent')
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012, decimals=0));
+plt.savefig('emergency.png', bbox_inches='tight', pad_inches=0.25)
+
+# Duty
+g = sns.catplot(x='duty', kind='count', palette='hls', data=df, height=6, aspect=10/6)
+plt.title('Duty At Time of Death', fontsize=18)
+plt.xlabel('Duty')
+plt.ylabel('Percent')
+[plt.setp(ax.get_xticklabels(), rotation=30) for ax in g.axes.flat]
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012, decimals=0));
+plt.savefig('duty.png', bbox_inches='tight', pad_inches=0.25)
+
+# Property Type
+g = sns.catplot(x='property_type', kind='count', palette='hls', data=df, height=6, aspect=10/6)
+plt.title('Type of Property', fontsize=18)
+plt.xlabel('Property Type')
+plt.ylabel('Percent')
+[plt.setp(ax.get_xticklabels(), rotation=30) for ax in g.axes.flat]
+plt.gca().yaxis.set_major_formatter(PercentFormatter(2012, decimals=0));
+plt.savefig('property_type.png', bbox_inches='tight', pad_inches=0.25)
+
+# Age
+# Remove the missing values from data set
+age_df = df.loc[df['age'] != -99]
+
+with sns.axes_style("ticks"):
+    sns.displot(data=age_df, x='age', height=6, aspect=10/6, kde=True, color='red')
+    plt.title('Ages of Firefighters', fontsize=18)
+    plt.xlabel('Age')
+    plt.ylabel('Count')
+    plt.savefig('age.png', bbox_inches='tight', pad_inches=0.25)
+
+# Date of Incident and Death
+with sns.axes_style("ticks"):
+    sns.displot(data=df, x='incident_date', height=6, aspect=10/6, kind='kde', color='red')
+    plt.title('Date of Incident', fontsize=18)
+    plt.xlabel('Year')
+    plt.ylabel('Count')
+    plt.savefig('date_incident_original.png', bbox_inches='tight', pad_inches=0.25)
+
+incident_df = df.copy()
+
+incident_df = add_datepart(incident_df, 'incident_date')
+
+with sns.axes_style("ticks"):
+    sns.displot(data=incident_df, x='incident_Year', height=6, aspect=10/6)
+    plt.title('Date of Incident', fontsize=18)
+    plt.xlabel('Year')
+    plt.ylabel('Count')
+    plt.savefig('date_incident_bins.png', bbox_inches='tight', pad_inches=0.25)
+
+dod_df = df.copy()
+dod_df = add_datepart(dod_df, 'date_of_death')
+
+with sns.axes_style("ticks"):
+    sns.displot(data=dod_df, x='date_of_deathYear', kind='kde', height=6, aspect=10/6)
+    plt.title('Date of Death', fontsize=18)
+    plt.xlabel('Year')
+    plt.ylabel('Count')
+    plt.savefig('date_death_bins.png', bbox_inches='tight', pad_inches=0.25)
+
+sns.boxplot(data=dod_df, x='date_of_deathDayofweek', height=6, aspect=10/6)
 plt.show()
